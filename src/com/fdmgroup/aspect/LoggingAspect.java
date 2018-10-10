7e50e3a6c8e552c022fbe93bc2929474e361a74e
jasdip.sohi <jasdip.sohi@fdmgroup.com>
2018-10-10 13:55:31 -0400

    Intitial Commit.

---
+++ src/com/fdmgroup/aspect/LoggingAspect.java
@@ -0,0 +1,15 @@
+package com.fdmgroup.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+	
+	private void emptyMethod(){}
+	
+	private void emptyMethodForThrow(){}
+	
+	private void profiler(){}
+	
+	
+}
---
+++ src/com/fdmgroup/service/ShoppingCart.java
@@ -0,0 +1,37 @@
+package com.fdmgroup.service;
+
+public class ShoppingCart {
+	
+	public void addItem(String name, int quantity){
+		System.out.println("Adding " + quantity + " unit(s) of " + name + " to the cart.");
+	}
+	
+	public void removeItem(String name, int quantity){
+		System.out.println("Removing " + quantity + " unit(s) of " + name + " from the cart.");
+	}
+	
+	public void remove(String name){
+		System.out.println("Removing all of the" + name + " from the cart.");
+	}
+	
+	public void removeAll(String name) throws Exception{
+		System.out.println("Removing everything from the cart.");
+		throw new Exception("Generic Exception");
+	}
+	
+	public int loop(){
+		int sum = 0;
+		for(int i = 1; i < 11; i++){
+			sum+=i;
+			System.out.println(i + ", ");
+			
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return sum;
+	}
+	
+}
