01b548a6462a58270c28ac8abfe92e338b786bf1
jasdip.sohi <jasdip.sohi@fdmgroup.com>
2018-10-10 14:17:08 -0400

    Added minor functionality

---
+++ src/com/fdmgroup/app/MainApp.java
@@ -0,0 +1,20 @@
+package com.fdmgroup.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.fdmgroup.service.ShoppingCart;
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		
+		ShoppingCart sc = context.getBean(ShoppingCart.class);
+		
+		sc.addItem("iphone", 10);
+		
+		System.out.println("===============================================");
+		
+	}
+}
--- src/com/fdmgroup/aspect/LoggingAspect.java
+++ src/com/fdmgroup/aspect/LoggingAspect.java
@@ -1,15 +1,35 @@
 package com.fdmgroup.aspect;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class LoggingAspect {
-	
+
 	private void emptyMethod(){}
-	
+
 	private void emptyMethodForThrow(){}
-	
+
 	private void profiler(){}
+
+	//Advice method
+	public void logBefore(JoinPoint jp){
+		System.out.println("logBefore is called before " + jp.getSignature().getName() + "----->"
+				+ Arrays.toString(jp.getArgs()));
+	}
+
+	//Advice method
+	public void logAfter(JoinPoint jp){
+		System.out.println("logAfter is called after " + jp.getSignature().getName() + "----->"
+				+ Arrays.toString(jp.getArgs()));
+	}
 	
-	
+	//Advice method
+	public void logAfterThrowing(JoinPoint jp){
+		System.out.println("logAfterThrowing is called after " + jp.getSignature().getName() + "----->"
+				+ Arrays.toString(jp.getArgs()));
+	}
+
 }
